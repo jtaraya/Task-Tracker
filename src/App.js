a008043116ea11b35f434055de01967e00d05c41
Jacob Taraya <jacobtaraya@gmail.com>
2023-01-12 17:04:37 +0300

    settask reminder true false

--- src/App.js
+++ src/App.js
@@ -31,7 +31,9 @@ const deleteTask = (id) => {
 
 //Toggle Reminder
 const toggleReminder = (id) => {
-  console.log(id)
+  setTasks(
+    tasks.map((task) => task.id ===id 
+  ? { ...task, reminder: !task.reminder } : task))
 }
 
   return (
