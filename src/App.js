e867d1a1cbdc31e903be068e6b250a46a503cb10
Jacob Taraya <jacobtaraya@gmail.com>
2023-01-12 18:29:40 +0300

    const id = Math.floor(Math.random()) * 10000 + 1

--- src/App.js
+++ src/App.js
@@ -27,7 +27,12 @@ const App = () => {
 
 //Add Task Submit
 const addTask = (task) => {
-  console.log(task)
+  const id = Math.floor(Math.random()) * 10000 + 1
+
+  //console.log(id)
+
+  const newtask = { id, ...task }
+  setTasks([...tasks, newtask])
 }
 
 
