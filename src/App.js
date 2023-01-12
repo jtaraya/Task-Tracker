bf86173b69e3c439243095fb1f00bb0d4c84bb60
Jacob Taraya <jacobtaraya@gmail.com>
2023-01-12 15:15:00 +0300

    message if no task

--- src/App.js
+++ src/App.js
@@ -26,13 +26,13 @@ const App = () => {
 
 //Delete Task
 const deleteTask = (id) => {
-  console.log('delete', id); // also log the id
+  setTasks(tasks.filter((task) => task.id !==id))
 }
 
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} /> 
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks To Show' }
     </div>
   );
 }
