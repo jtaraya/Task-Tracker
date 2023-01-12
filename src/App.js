ff98acedd7d0ecb80906db35e9b9f2738b76c4f9
Jacob Taraya <jacobtaraya@gmail.com>
2023-01-12 18:01:59 +0300

    Add task Submit event

--- src/App.js
+++ src/App.js
@@ -25,6 +25,12 @@ const App = () => {
     },
 ])
 
+//Add Task Submit
+const addTask = (task) => {
+  console.log(task)
+}
+
+
 //Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !==id))
@@ -40,7 +46,7 @@ const toggleReminder = (id) => {
   return (
     <div className='container'>
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
           ) : ( 
